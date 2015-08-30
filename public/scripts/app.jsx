3ead5e185145ae504a74f2bca06036263aac66c4
Pete Petrash <pete.petrash@gmail.com>
2015-08-30 00:08:18 -0700

    cleanup

--- public/scripts/app.jsx
+++ public/scripts/app.jsx
@@ -1,64 +1,64 @@
 
 var Food = React.createClass({
-  render: function() {
-    return (
-      <div className="Food" data-type={this.props.type}     style={{backgroundColor: this.props.hue}}>
-        <span className="foodName">
-          {this.props.name}
-        </span>
-      </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="Food" data-type={this.props.type}     style={{backgroundColor: this.props.hue}}>
+                <span className="foodName">
+                    {this.props.name}
+                </span>
+            </div>
+        );
+    }
 });
 
 var FoodCart = React.createClass({
-  loadFoodsFromServer: function() {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
+    loadFoodsFromServer: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
   },
   handleFoodSubmit: function(food) {
-    var foods = this.state.data;
-    foods.push(food);
-    this.setState({data: foods}, function() {
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        type: 'POST',
-        data: food,
-        success: function(data) {
-          this.setState({data: data});
-        }.bind(this),
-        error: function(xhr, status, err) {
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
+      var foods = this.state.data;
+      foods.push(food);
+      this.setState({data: foods}, function() {
+          $.ajax({
+              url: this.props.url,
+              dataType: 'json',
+              type: 'POST',
+              data: food,
+              success: function(data) {
+                  this.setState({data: data});
+              }.bind(this),
+              error: function(xhr, status, err) {
+                  console.error(this.props.url, status, err.toString());
+              }.bind(this)
+          });
       });
-    });
   },
   getInitialState: function() {
-    return {data: []};
+      return {data: []};
   },
   componentDidMount: function() {
-    this.loadFoodsFromServer();
-    setInterval(this.loadFoodsFromServer, this.props.pollInterval);
+      this.loadFoodsFromServer();
+      setInterval(this.loadFoodsFromServer, this.props.pollInterval);
   },
   render: function() {
-    return (
-      <div className="FoodCart">
-        <FilterFoodsForm onFilterSelect={this.applyFilter}/>
-        <AddFoodForm onFoodSubmit={this.handleFoodSubmit} />
-        <FoodList data={this.state.data} />
-      </div>
-    );
-  }
+        return (
+            <div className="FoodCart">
+                <FilterFoodsForm onFilterSelect={this.applyFilter}/>
+                <AddFoodForm onFoodSubmit={this.handleFoodSubmit} />
+                <FoodList data={this.state.data} />
+            </div>
+        );
+      }
 });
 
 
@@ -76,7 +76,7 @@ var FoodList = React.createClass({
         foodNodes = sortedArray.map(function(food, index) {
             return (
               <Food key={index} name={food.name} type={food.type} hue={food.hue}>
-                {food.text}
+                  {food.text}
               </Food>
             );
         });
@@ -90,29 +90,29 @@ var FoodList = React.createClass({
 });
 
 var AddFoodForm = React.createClass({
-  handleSubmit: function(e) {
-    e.preventDefault();
-    var name = React.findDOMNode(this.refs.name).value.trim();
-    var type = React.findDOMNode(this.refs.type).value.trim();
-    var hue = React.findDOMNode(this.refs.hue).value.trim();
-    if (!name || !type || !hue) {
-      return;
-    }
-    this.props.onFoodSubmit({name: name, type: type, hue: hue});
-    React.findDOMNode(this.refs.name).value = '';
-    React.findDOMNode(this.refs.type).value = '';
-    React.findDOMNode(this.refs.hue).value = '';
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var name = React.findDOMNode(this.refs.name).value.trim();
+        var type = React.findDOMNode(this.refs.type).value.trim();
+        var hue = React.findDOMNode(this.refs.hue).value.trim();
+        if (!name || !type || !hue) {
+            return;
+        }
+        this.props.onFoodSubmit({name: name, type: type, hue: hue});
+        React.findDOMNode(this.refs.name).value = '';
+        React.findDOMNode(this.refs.type).value = '';
+        React.findDOMNode(this.refs.hue).value = '';
   },
   render: function() {
-    return (
-      <form className="AddFoodForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Name" ref="name" />
-        <input type="text" placeholder="Type" ref="type" />
-        <input type="text" placeholder="Hue" ref="hue" />
-        <input type="submit" value="Add Food" />
-      </form>
-    );
-  }
+        return (
+            <form className="AddFoodForm" onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Name" ref="name" />
+                <input type="text" placeholder="Type" ref="type" />
+                <input type="text" placeholder="Hue" ref="hue" />
+                <input type="submit" value="Add Food" />
+            </form>
+        );
+    }
 });
 
 var FilterFoodsForm = React.createClass({
@@ -122,30 +122,33 @@ var FilterFoodsForm = React.createClass({
     handleSubmit: function() {
         console.log("hi");
     },
-  render: function() {
-    return (
-      <form className="FilterFoodsForm" onChange={this.handleChange}>
-        <select defaultValue="Fruits">
-            <option value="fruits">Fruits</option>
-            <option value="vegetables">Vegetables</option>
-            <option value="herbs">Herbs</option>
-        </select>
-        <select defaultValue="Arkansas">
-            <option value="Arkansas">Arkansas</option>
-            <option value="California">California</option>
-            <option value="Texas">Texas</option>
-        </select>
-        <select defaultValue="January" selected={this.handleChange}>
-            <option value="January" >January</option>
-            <option value="February">February</option>
-            <option value="March">March</option>
-        </select>
-      </form>
-    );
-  }
+    render: function() {
+      return (
+          <form className="FilterFoodsForm" onChange={this.handleChange}>
+
+                <select defaultValue="Fruits">
+                    <option value="fruits">Fruits</option>
+                    <option value="vegetables">Vegetables</option>
+                    <option value="herbs">Herbs</option>
+                </select>
+
+                <select defaultValue="Arkansas">
+                    <option value="Arkansas">Arkansas</option>
+                    <option value="California">California</option>
+                    <option value="Texas">Texas</option>
+                </select>
+
+                <select defaultValue="January" selected={this.handleChange}>
+                    <option value="January" >January</option>
+                    <option value="February">February</option>
+                    <option value="March">March</option>
+                </select>
+            </form>
+        );
+    }
 });
 
 React.render(
-  <FoodCart url="foods.json" pollInterval={2000} />,
-  document.getElementById('content')
+    <FoodCart url="foods.json" pollInterval={2000} />,
+    document.getElementById('content')
 );
